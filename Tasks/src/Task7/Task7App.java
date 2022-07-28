ed166251a3485abea0940f72b5df396462adcb19
ionela vladasel <ionelavladasel2710@gmail.com>
2022-07-28 13:51:13 +0200

    added task7

--- Tasks/src/Task7/Task7App.java
+++ Tasks/src/Task7/Task7App.java
@@ -6,16 +6,42 @@ public class Task7App {
 
 	public static void main(String[] args) {
 		Mysql_interconnect my_conection = new Mysql_interconnect();
+		// conectamos
 		my_conection.connect("192.168.5.104", "3306", "remote", "27ione91");
+		// Creamos la base de datos
 		my_conection.createDB("ud18_7");
+		// elegimos la base de datos
 		my_conection.useDB("ud18_7");
 
-		my_conection.createTable("create table cientificos(" + "DNI varchar(8)," + "nom_apell varchar(255),"
-				+ "primary key(DNI)" + ");");
+		// Creamos las tablas
 
-		my_conection.createTable("create table proyectos();");
+		my_conection.createTable("create table if not exists cientificos(" + "DNI varchar(8),"
+				+ "nom_apell varchar(255)," + "primary key(DNI)" + ");");
 
-		my_conection.createTable("create table asignado_a();");
+		my_conection.createTable(
+				"create table if not exists proyectos(id char(4),nombre varchar(255),horas int, primary key(id));");
+
+		my_conection.createTable(
+				"create table if not exists asignado_a(cientifico varchar(8),proyecto char(4),primary key(cientifico,proyecto),"
+						+ "constraint fk_asignado_cientificos foreign key(cientifico) references cientificos(DNI)"
+						+ "on delete cascade on update cascade,"
+						+ "constraint fk_asignado_proyectos foreign key(proyecto) references proyectos(id)"
+						+ "on delete cascade on update cascade);");
+
+		// hacemos inserts
+		my_conection.insertRecord("insert into cientificos(DNI,nom_apell) values" + "('1111111a','pepito grillo'),"
+				+ "('2222222a','mari carmen lara')," + "('3333333a','pollito piolin'),"
+				+ "('4444444a','consuelo martinez')," + "('5555555a','joselito perez');");
+
+		my_conection.insertRecord("insert into proyectos(id,nombre,horas) values ('123a','explotar una estrella',987),"
+				+ "('123b','reproduccion mariposas',45)," + "('123c','analisis conducta hormigas',675),"
+				+ "('123d','patron y diseño casas sostenibles',564)," + "('123e','canto sirenas analisis',452);");
+
+		my_conection.insertRecord("insert into asignado_a(cientifico,proyecto) values('1111111a','123a'),"
+				+ "('2222222a','123b')," + "('3333333a','123c')," + "('4444444a','123d')," + "('5555555a','123e');");
+
+		my_conection.closeConnection();
+
 	}
 
 }
