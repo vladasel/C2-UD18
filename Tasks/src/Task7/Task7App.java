13fb3ab995f498f01fb7061f1eef8f3ca5148439
ionela vladasel <ionelavladasel2710@gmail.com>
2022-07-28 12:17:54 +0200

    before pull

--- Tasks/src/Task7/Task7App.java
+++ Tasks/src/Task7/Task7App.java
@@ -1,8 +1,21 @@
 package Task7;
 
+import mysql_interconnect.Mysql_interconnect;
+
 public class Task7App {
 
 	public static void main(String[] args) {
+		Mysql_interconnect my_conection = new Mysql_interconnect();
+		my_conection.connect("192.168.5.104", "3306", "remote", "27ione91");
+		my_conection.createDB("ud18_7");
+		my_conection.useDB("ud18_7");
+
+		my_conection.createTable("create table cientificos(" + "DNI varchar(8)," + "nom_apell varchar(255),"
+				+ "primary key(DNI)" + ");");
+
+		my_conection.createTable("create table proyectos();");
+
+		my_conection.createTable("create table asignado_a();");
 	}
 
 }
--- Tasks/src/Task8/Task8App.java
+++ Tasks/src/Task8/Task8App.java
@@ -1,8 +1,20 @@
 package Task8;
 
+import mysql_interconnect.Mysql_interconnect;
+
 public class Task8App {
 
 	public static void main(String[] args) {
+		Mysql_interconnect my_conection = new Mysql_interconnect();
+		my_conection.connect("192.168.5.104", "3306", "remote", "27ione91");
+		my_conection.createDB("ud18_8");
+		my_conection.useDB("ud18_8");
+		
+		my_conection.createTable("");
+		my_conection.createTable("");
+		my_conection.createTable("");
+		my_conection.createTable("");
+		
 	}
 
 }
