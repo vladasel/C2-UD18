96f843cc7fde06ba1f313ced0ee318bff8cdb976
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-07-28 10:25:36 +0200

    mysql_interconnect: Start adding functions for handling MySQL database-related processes

--- Task1/src/mysql_interconnect/Mysql_interconnect.java
+++ Task1/src/mysql_interconnect/Mysql_interconnect.java
@@ -1,5 +1,23 @@
 package mysql_interconnect;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Mysql_interconnect {
 
+	public void connect(String m_server, String m_port, String m_user, String m_password) {
+		try {
+	        Class.forName("com.mysql.cj.jdbc.Driver");
+	        Connection m_connection = DriverManager.getConnection(
+	        		"jdbc:mysql://" + m_server + ":" + m_port + "?useTimezone=true&serverTimezone=UTC",
+	        		m_user, m_password);
+	        System.out.println("Server Connected");
+	
+	    } catch (SQLException | ClassNotFoundException e) {
+	        System.out.println("No se ha podido conectar con mi base de datos");
+	        System.out.println(e);
+	    }
+	}
+	
 }
