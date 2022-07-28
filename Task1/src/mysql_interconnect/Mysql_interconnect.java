09da99a51e23a49806b19fa26fd9c07f7353eee8
ionela vladasel <ionelavladasel2710@gmail.com>
2022-07-28 10:22:06 +0200

    none

--- Task1/src/mysql_interconnect/Mysql_interconnect.java
+++ Task1/src/mysql_interconnect/Mysql_interconnect.java
@@ -2,4 +2,23 @@ package mysql_interconnect;
 
 public class Mysql_interconnect {
 
+	//abrir conexion
+	
+	//cerrar conexion
+	
+	//crear bd
+	
+	//eliminar bd
+	
+	//crear tabla sin datos
+	
+	//crear tabla con datos
+	
+	//update tabla
+	
+	//update columna
+	
+	//eliminar columna
+	
+	//eliminar tabla
 }
