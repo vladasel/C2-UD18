8330d2086384ade58c25cd048742235253cba3cf
ionela vladasel <ionelavladasel2710@gmail.com>
2022-07-28 10:42:42 +0200

    added method createDB(); git pull

--- Task1/src/mysql_interconnect/Mysql_interconnect.java
+++ Task1/src/mysql_interconnect/Mysql_interconnect.java
@@ -8,35 +8,47 @@ public class Mysql_interconnect {
 
 	public void connect(String m_server, String m_port, String m_user, String m_password) {
 		try {
-	        Class.forName("com.mysql.cj.jdbc.Driver");
-	        Connection m_connection = DriverManager.getConnection(
-	        		"jdbc:mysql://" + m_server + ":" + m_port + "?useTimezone=true&serverTimezone=UTC",
-	        		m_user, m_password);
-	        System.out.println("Server Connected");
-	
-	    } catch (SQLException | ClassNotFoundException e) {
-	        System.out.println("No se ha podido conectar con mi base de datos");
-	        System.out.println(e);
-	    }
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection m_connection = DriverManager.getConnection(
+					"jdbc:mysql://" + m_server + ":" + m_port + "?useTimezone=true&serverTimezone=UTC", m_user,
+					m_password);
+			System.out.println("Server Connected");
+
+		} catch (SQLException | ClassNotFoundException e) {
+			System.out.println("No se ha podido conectar con mi base de datos");
+			System.out.println(e);
+		}
 	}
-	
-	//abrir conexion
-	
-	//cerrar conexion
-	
-	//crear bd
-	
-	//eliminar bd
-	
-	//crear tabla sin datos
-	
-	//crear tabla con datos
-	
-	//update tabla
-	
-	//update columna
-	
-	//eliminar columna
-	
-	//eliminar tabla
+
+	// abrir conexion
+
+	// cerrar conexion
+
+	// crear bd
+	public void createDB(String name) {
+		try {
+			String query = "CREATE DATABASE " + name;
+			Statement at = Conexion.createStatement();
+			at.executeUpdate(query);
+			closeConnection();
+			MySQLConnection("root", "", name);
+			JOptionPane.showMessageDialog(null, "se a creado la base de datos " + name + " exitosamente");
+		} catch (SQLExceptioin ex) {
+			Logger.getLogger(MySQL.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
+	// eliminar bd
+
+	// crear tabla sin datos
+
+	// crear tabla con datos
+
+	// update tabla
+
+	// update columna
+
+	// eliminar columna
+
+	// eliminar tabla
 }
