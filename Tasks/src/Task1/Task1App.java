6da93f0fc042383772d5e3840fae727def0b88ab
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-08-04 18:17:12 +0200

    Task1: Upload finished Task 1 w/Inserts

--- Tasks/src/Task1/Task1App.java
+++ Tasks/src/Task1/Task1App.java
@@ -9,32 +9,37 @@ import mysql_interconnect.Mysql_interconnect;
 public class Task1App {
 
 	public static void main(String[] args) {
-		Mysql_interconnect m_conection = new Mysql_interconnect();
+		Mysql_interconnect m_connection = new Mysql_interconnect();
+		m_connection.connect("IP", "PORT", "USER", "PASSWORD");
+		m_connection.createDB("Ud18_Ej4");
+		m_connection.useDB("Ud18_Ej4");
 
-		/*
-		 * String m_host = JOptionPane.showInputDialog("Type the hostname"); String
-		 * m_port = JOptionPane.showInputDialog("Type the port"); String m_user =
-		 * JOptionPane.showInputDialog("Type " + m_host + "'s username");
-		 * 
-		 * JFrame m_frame = new JFrame(); JPasswordField m_pwdfld = new
-		 * JPasswordField(); m_pwdfld.setEchoChar('*'); m_pwdfld.setColumns(32);
-		 * 
-		 * JOptionPane.showConfirmDialog(m_frame, m_pwdfld, "Type " + m_user +
-		 * "'s password", JOptionPane.OK_CANCEL_OPTION );
-		 * 
-		 * JOptionPane.showMessageDialog(null, "Connecting to " + m_host + ":" + m_port
-		 * + " -> " + m_user + "@" + String.valueOf(m_pwdfld.getPassword()));
-		 * 
-		 * m_conection.connect(m_host, m_port, m_user,
-		 * String.valueOf(m_pwdfld.getPassword()));
-		 */
-
-		m_conection.connect("", "", "", "");
-
-		m_conection.createDB("ud19");
-
-		m_conection.createTable("");
-
+		m_connection.createTable("create table if not exists fabricante ("		+
+										"identity int primary key not null, "	+
+										"nombre nvarchar(100) );"
+								);
+		
+		
+		m_connection.createTable("create table articulos ("						+
+										"identity int primary key not null, "	+
+									    "nombre nvarchar(100), "				+
+									    "precio int, "							+
+									    "fabricante int, "						+
+									    "constraint fk_fabricante foreign key(fabricante) references fabricante(identity) );"
+								);
+		
+		m_connection.insertRecord("insert into fabricante(identity, nombre) values (1, 'Philips')");
+		m_connection.insertRecord("insert into fabricante(identity, nombre) values (2, 'Sony')");
+		m_connection.insertRecord("insert into fabricante(identity, nombre) values (3, 'Microsoft')");
+		m_connection.insertRecord("insert into fabricante(identity, nombre) values (4, 'Nintendo')");
+		m_connection.insertRecord("insert into fabricante(identity, nombre) values (5, 'Amiga')");
+		
+		m_connection.insertRecord("insert into articulos(identity, nombre, precio, fabricante) values (0, 'Head Screwdriver', 10, 1)");
+		m_connection.insertRecord("insert into articulos(identity, nombre, precio, fabricante) values (2, 'PlayStation 2', 300, 2)");
+		m_connection.insertRecord("insert into articulos(identity, nombre, precio, fabricante) values (3, 'Xbox Classic', 350, 3)");
+		m_connection.insertRecord("insert into articulos(identity, nombre, precio, fabricante) values (4, 'NES', 250, 4)");
+		m_connection.insertRecord("insert into articulos(identity, nombre, precio, fabricante) values (5, '500', 550, 5)");
+		
 		m_conection.closeConnection();
 	}
 
